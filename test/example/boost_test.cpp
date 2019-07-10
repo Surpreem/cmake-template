b12308e96c3f807a919c1d700fccb5c94777b093
Surpreem <surpreem+sn@gmail.com>
2019-07-11 08:57:56 +0900

    Add Boost C++ Libraries

---
+++ test/example/boost_test.cpp
@@ -0,0 +1,22 @@
+﻿#define CATCH_CONFIG_MAIN
+
+#include "boost/thread.hpp"
+#include "catch2/catch.hpp"
+
+
+TEST_CASE("boost.thread")
+{
+    // GIVEN
+    auto value{0};
+
+    // WHEN
+    auto t{boost::thread{[&value] {
+        for (auto i{0}; 10 != i; ++i)
+            ++value;
+    }}};
+
+    t.join();
+
+    // THEN
+    REQUIRE(10 == value);
+}
